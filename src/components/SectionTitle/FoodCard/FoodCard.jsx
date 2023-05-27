646c50c4684905f4d6ca735038c28c4ddceae66f
siddiquenaim <naim.niter.tex06@gmail.com>
2023-05-27 17:49:44 +0600

    added food card data

---
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const FoodCard = ({ item }) => {
+  const { name, image, price, recipe } = item;
+  return (
+    <div className="card w-full bg-[#F3F3F3] rounded-none">
+      <figure>
+        <img className="w-[424px] h-[300px]" src={image} alt="Shoes" />
+      </figure>
+      <div className="card-body text-center">
+        <h2 className="text-center">{name}</h2>
+        <p>Price: {price}</p>
+        <p>Recipe: {recipe}</p>
+        <div className=" justify-end text-center">
+          <button className="btn btn-outline text-[#BB8506] border-0 border-b-4 border-[#BB8506] mt-6 hover:bg-black hover:text-[#BB8506]">
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,9 +1,75 @@
-import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import orderImage from "../../../assets/shop/banner2.jpg";
+import MenuCategory from "../../Menu/MenuCategory/MenuCategory";
+import { useState } from "react";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../hooks/useMenu";
+import FoodCard from "../../../components/SectionTitle/FoodCard/FoodCard";
 
 const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const drinks = menu.filter((item) => item.category === "drinks");
+
   return (
     <div>
-      <h3>Orders will be coming here</h3>
+      <MenuCategory
+        coverImage={orderImage}
+        title={"Order Food"}
+        desc={"Would you like to try a dish?"}
+      ></MenuCategory>
+      <Tabs
+        className="w-[90%] mx-auto my-20"
+        defaultIndex={tabIndex}
+        onSelect={(index) => setTabIndex(index)}
+      >
+        <TabList className="text-center mb-4">
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Dessert</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel>
+          <div className="grid md:grid-cols-3 gap-10">
+            {salad.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid md:grid-cols-3 gap-10">
+            {pizza.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid md:grid-cols-3 gap-10">
+            {soup.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid md:grid-cols-3 gap-10">
+            {dessert.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid md:grid-cols-3 gap-10">
+            {drinks.map((item) => (
+              <FoodCard key={item._id} item={item}></FoodCard>
+            ))}
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
